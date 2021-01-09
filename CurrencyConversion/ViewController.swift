2df8884ad42f5f3f7fdcb08e946129fb35377e46
Henrikas <henrikasju@gmail.com>
2021-01-09 14:12:31 +0200

    Initial Commit

---
+++ CurrencyConversion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyConversion
+//
+//  Created by Henrikas J on 09/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
