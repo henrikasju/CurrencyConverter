d1af776efda28a22385ed6ebb00d79ce1ddc1bf4
Henrikas <henrikasju@gmail.com>
2021-01-09 17:54:34 +0200

    Creating CurrencyConversion main view

---
+++ CurrencyConversion/Scenes/CurrencyConverter/UIElements/CurrencyConverterView.swift
@@ -0,0 +1,18 @@
+//
+//  CurrencyConverterView.swift
+//  CurrencyConversion
+//
+//  Created by Henrikas J on 09/01/2021.
+//
+
+import UIKit
+
+class CurrencyConverterView: UIView {
+
+  convenience init() {
+    self.init(frame: .zero)
+
+    backgroundColor = .blue
+  }
+
+}
